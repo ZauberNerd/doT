7172c34060a9a695ae150b1acef026611966185b
ZauberNerd <zaubernerd@zaubernerd.de>
2014-06-21 20:25:40 +0200

    renamed 'it' to 'ctx'

--- doT.js
+++ doT.js
@@ -14,7 +14,7 @@ var doT = {
         defineParams:/^\s*([\w$]+):([\s\S]+)/,
         conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
         iterate:     /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
-        varname:    'it',
+        varname:    'ctx',
         strip:      true,
         append:     true
     },
