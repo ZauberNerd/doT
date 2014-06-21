a7e630a3d7660279643bc0113db6aa42b9c35974
ZauberNerd <zaubernerd@zaubernerd.de>
2014-06-21 19:24:17 +0200

    use spaces instead of tabs

--- runtime.js
+++ runtime.js
@@ -1,10 +1,10 @@
 (function () {
-	function encodeHTMLSource() {
-		var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", "\"": "&#34;", "'": "&#39;", "`": "&#96;", "\/": "&#47;" }
-			matchHTML = /&(?!#?\w+;)|<|>|"|'|\//g;
-		return function() {
-			return this ? this.replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : this;
-		};
-	}
-	String.prototype._doTencodeHTML = encodeHTMLSource();
+    function encodeHTMLSource() {
+        var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", "\"": "&#34;", "'": "&#39;", "`": "&#96;", "\/": "&#47;" }
+            matchHTML = /&(?!#?\w+;)|<|>|"|'|\//g;
+        return function() {
+            return this ? this.replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : this;
+        };
+    }
+    String.prototype._doTencodeHTML = encodeHTMLSource();
 }());
