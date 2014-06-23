134aa50f1e9342c1dcced1ab18b91fcb28080ef9
ZauberNerd <zaubernerd@zaubernerd.de>
2014-06-23 18:39:51 +0200

    changed templateSettings to defaults

--- examples/customdoT.js
+++ examples/customdoT.js
@@ -3,7 +3,7 @@
 		data = { f1: 1, f2: 2, f3: 3},
 		snippet = '<h1>Just static text</h1> <p>Here is a simple case <?=it.f1+it.f3?> </p> <div class="<?=it.f1?>"> Next we will use a JavaScript block: </div> <? for(var i=0; i < it.f2; i++) { ?>	<div><?=it.f3?></div> <? }; ?>';
 
-	doT.templateSettings = {
+	doT.defaults = {
 		evaluate : /\<\?([\s\S]+?)\?\>/g,
 		interpolate : /\<\?=([\s\S]+?)\?\>/g,
 		varname     : 'it',
